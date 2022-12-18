7b220eb8920726788677f1e67bb5646a984748a2
NateZimmer <NateZimmer@users.noreply.github.com>
2022-12-18 03:10:29 +0000

    [create-pull-request] automated change

--- src/ring_buf/ring_buf.h
+++ src/ring_buf/ring_buf.h
@@ -2,52 +2,52 @@
 #ifndef RING_BUF_H
 #define RING_BUF_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef struct
 {
-  uint8_t * data;
-  uint8_t * head;
-  uint8_t * tail;
+  uint8_t *data;
+  uint8_t *head;
+  uint8_t *tail;
   uint16_t total_size;
-}ring_buf_t;
+} ring_buf_t;
 
 /**
  * @brief initialize ring buffer
  * @param[in] buf - Pointer to ring buf struct, see ring_buf_t
- * @param[in] len - Length of buffer in bytes 
+ * @param[in] len - Length of buffer in bytes
  */
-void ring_buf_init(ring_buf_t * buf, uint16_t len);
+void ring_buf_init(ring_buf_t *buf, uint16_t len);
 
 /**
- * @brief gets the length of bytes in buffer 
+ * @brief gets the length of bytes in buffer
  * @param[in] buf - Pointer to ring buf struct, see ring_buf_t
  */
-uint16_t ring_buf_len(ring_buf_t * buf);
+uint16_t ring_buf_len(ring_buf_t *buf);
 
 /**
- * @brief add bytes into ring buffer 
+ * @brief add bytes into ring buffer
  * @param[in] buf - Pointer to ring buf struct, see ring_buf_t
  * @param[in] data - Pointer to buffer to be filled into ring buffer
  * @param[in] len - length of data in bytes
- * @return overflow, true if an overflow occurred 
+ * @return overflow, true if an overflow occurred
  */
-bool ring_buf_add(ring_buf_t * buf, uint8_t * data, uint16_t len);
+bool ring_buf_add(ring_buf_t *buf, uint8_t *data, uint16_t len);
 
 /**
- * @brief get bytes from ring buffer 
+ * @brief get bytes from ring buffer
  * @param[in] buf - Pointer to ring buf struct, see ring_buf_t
  * @param[out] data - Pointer to buffer to be filled into ring buffer
  * @param[in] len - length of data in bytes
  * @return bytes read, will be less than len if amount in buffer is less than len
  */
-uint16_t ring_buf_get(ring_buf_t * buf, uint8_t * data, uint16_t len);
+uint16_t ring_buf_get(ring_buf_t *buf, uint8_t *data, uint16_t len);
 
 /**
  * @brief clears out data in buffer
  * @param[in] buf - Pointer to ring buf struct, see ring_buf_t
  */
-void ring_buf_reset(ring_buf_t * buf); 
+void ring_buf_reset(ring_buf_t *buf);
 
 #endif // RING_BUF_H
--- test/pcap_test.c
+++ test/pcap_test.c
@@ -2,16 +2,15 @@
 #define TEST_NO_MAIN
 #include "acutest.h"
 
-#include "pcap_test.h"
 #include "../src/pcap_util/pcap_util.h"
+#include "pcap_test.h"
 
 void test_pcap()
 {
-    FILE * fp;
-    uint8_t data[4] = {1, 2, 3, 4}; 
-    fp = pcap_initialize(NULL, 147);
-    pcap_write_packet(data, sizeof(data), &fp);
-    pcap_write_packet(data, sizeof(data), &fp);
-    TEST_CHECK( fp != NULL);
+  FILE *fp;
+  uint8_t data[4] = {1, 2, 3, 4};
+  fp = pcap_initialize(NULL, 147);
+  pcap_write_packet(data, sizeof(data), &fp);
+  pcap_write_packet(data, sizeof(data), &fp);
+  TEST_CHECK(fp != NULL);
 }
-
