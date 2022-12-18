61a301c23e7d3d839cb13f5603136208b8c24d3c
NateZimmer <NateZimmer@users.noreply.github.com>
2022-12-18 03:17:19 +0000

    [create-pull-request] automated change

--- test/ring_test.c
+++ test/ring_test.c
@@ -2,11 +2,11 @@
 #define TEST_NO_MAIN
 #include "acutest.h"
 
+#include "../src/ring_buf/ring_buf.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include "../src/ring_buf/ring_buf.h"
 
 ring_buf_t buf;
 
@@ -21,18 +21,18 @@ void ring_test()
   TEST_CHECK(ring_buf_len(&buf) == 0);
 
   printf("\r\ninitially buf len should be 0, buf len is %d, buf size is %d\r\n", ring_buf_len(&buf), buf.total_size);
-  overflow = ring_buf_add(&buf, (uint8_t []){1,2,3,4}, 4);
+  overflow = ring_buf_add(&buf, (uint8_t[]){1, 2, 3, 4}, 4);
   printf("adding {1,2,3,4}, buf len is %d, overflow: %d\r\n", ring_buf_len(&buf), overflow);
-  
+
   // Test Length, 4
   TEST_CHECK(ring_buf_len(&buf) == 4);
-  
-  overflow = ring_buf_add(&buf, (uint8_t []){5,6,7}, 3);
+
+  overflow = ring_buf_add(&buf, (uint8_t[]){5, 6, 7}, 3);
   printf("adding {5,6,7}, buf len is %d, overflow: %d\r\n", ring_buf_len(&buf), overflow);
   printf("Buf is: ");
   for(int i = 0; i < buf.total_size; i++)
   {
-    printf("%02x ",buf.data[i]);
+    printf("%02x ", buf.data[i]);
   }
   printf("\r\n");
   bytes_read = ring_buf_get(&buf, data, 3);
@@ -42,7 +42,7 @@ void ring_test()
     printf("%02x ", data[i]);
   }
   printf("\r\n");
-  overflow = ring_buf_add(&buf, (uint8_t []){1,2,3,4}, 4);
+  overflow = ring_buf_add(&buf, (uint8_t[]){1, 2, 3, 4}, 4);
   printf("adding {1,2,3,4}, buf len is %d, overflow: %d\r\n", ring_buf_len(&buf), overflow);
   bytes_read = ring_buf_get(&buf, data, 2);
   printf("reading 2 bytes, read %d\r\nBuf: ", bytes_read);
