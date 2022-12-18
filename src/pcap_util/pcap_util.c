5f806ed229da5ea48d498a11fd1ae205817a3907
NateZimmer <NateZimmer@users.noreply.github.com>
2022-12-18 02:43:17 +0000

    [create-pull-request] automated change

--- src/pcap_util/pcap_util.c
+++ src/pcap_util/pcap_util.c
@@ -5,10 +5,10 @@
 
 #define PRINT_FNC printf
 
-static void pcap_get_packet_header(uint8_t * buf, uint32_t link_type)
+static void pcap_get_packet_header(uint8_t *buf, uint32_t link_type)
 {
   pcap_header_t header = {0};
-  uint8_t * data = NULL;
+  uint8_t *data = NULL;
   header.magic_number = 0xa1b2c3d4;
   header.version_major = 2;
   header.version_minor = 4;
@@ -23,19 +23,19 @@ static void pcap_get_packet_header(uint8_t * buf, uint32_t link_type)
 /**
     @brief Creates a .pcap file and returns an open file handle
     @param[in] pcap_file_name - file name pointer. Set to NULL for a timestamp auto-gen filename
-    @param[in] link_type - Wireshark link types. This defines what dissector is used when opening a .pcap  
+    @param[in] link_type - Wireshark link types. This defines what dissector is used when opening a .pcap
 */
-FILE * pcap_initialize(char * pcap_file_name, uint16_t link_type)
+FILE *pcap_initialize(char *pcap_file_name, uint16_t link_type)
 {
   uint8_t header_buf[PCAP_HEADER_SIZE] = {0};
   char file_name[50];
   struct timeval te;
-  FILE * fp = NULL;
+  FILE *fp = NULL;
 
   if(pcap_file_name == NULL)
   {
     gettimeofday(&te, NULL);
-    sprintf(file_name, "%d.pcap",(int)te.tv_sec);
+    sprintf(file_name, "%d.pcap", (int)te.tv_sec);
     fp = fopen(file_name, "ab+");
   }
   else
@@ -58,19 +58,19 @@ FILE * pcap_initialize(char * pcap_file_name, uint16_t link_type)
 
 /**
     @brief Appends a packet to a pcap file
-    @param[in] buf - Pointer to a byte array 
+    @param[in] buf - Pointer to a byte array
     @param[in] buf_len - Length of byte array
-    @param[in] fp - Pointer to pcap file pointer, use the output of &pcap_initialize  
+    @param[in] fp - Pointer to pcap file pointer, use the output of &pcap_initialize
 */
-void pcap_write_packet(uint8_t * buf, uint32_t buf_len, FILE ** fp)
+void pcap_write_packet(uint8_t *buf, uint32_t buf_len, FILE **fp)
 {
   struct timeval te;
-  uint8_t pcap_header[PCAP_OFFSET] = { 0 };
-  uint8_t * index = NULL;
+  uint8_t pcap_header[PCAP_OFFSET] = {0};
+  uint8_t *index = NULL;
 
   gettimeofday(&te, NULL);
 
-  // create frame header 
+  // create frame header
   index = pcap_header;
   uint32_t num = te.tv_sec;
   memcpy(index, &num, sizeof(num));
--- test/test_master.c
+++ test/test_master.c
@@ -5,7 +5,5 @@
 #include "ring_test.h"
 
 TEST_LIST = {
-    { "PCAP Utils", test_pcap },
-    { "Ring Buf", ring_test },
-    { NULL, NULL }     /* zeroed record marking the end of the list */
+    {"PCAP Utils", test_pcap}, {"Ring Buf", ring_test}, {NULL, NULL} /* zeroed record marking the end of the list */
 };
